f37ea301934efdf3ac3041e33e9a6d2dd0ba25a1
Ayedhha <Ayedh1986@hotmail.com>
2025-06-19 21:28:41 +0300

    9

--- site_content/urls.py
+++ site_content/urls.py
@@ -5,6 +5,14 @@ from .views import home_view, signup_view
 urlpatterns = [
     path('', home_view, name='home'),  # الصفحة الرئيسية
     path('signup/', signup_view, name='signup'),  # تسجيل الحساب
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # تسجيل الدخول
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),  # تسجيل الخروج (اختياري)
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='site_content/login.html'),
+        name='login'
+    ),  # تسجيل الدخول باستخدام القالب المخصص
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='home'),
+        name='logout'
+    ),  # تسجيل الخروج
 ]
